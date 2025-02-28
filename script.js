4bc2676de7b0ae07b5716629caf81c8efbeeb4e8
Sergey <sergeynskvwork@gmail.com>
2025-02-28 17:26:55 +0500

    tg

--- script.js
+++ script.js
@@ -169,10 +169,10 @@ document.addEventListener('DOMContentLoaded', () => {
     initGame();
 
     function gameOver() {
+        alert()
         Telegram.WebApp.ready();
         Telegram.WebApp.sendData(JSON.stringify({ score: score }));
         gameContainer.style.backgroundColor = 'rgba(0, 0, 255, 0.7)';
         restartButton.style.display = 'block';
-        alert()
     }
 });
